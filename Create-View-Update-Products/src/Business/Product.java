4cbcc7aca65dd58164216bfb9290ceab0389f8b2
Tanmayee Venkata Laxmi Kalluri <kalluri.t@husky.neu.edu>
2018-10-29 22:32:58 -0400

    Create, View and Update Product Information

---
+++ Create-View-Update-Products/src/Business/Product.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business;
+
+/**
+ *
+ * @author Tanmayee Kalluri
+ */
+public class Product {
+    private String name;
+    private String price;
+    private String availNum;
+    private String description;
+    private Supplier supplier;
+    
+    public Product(){
+    supplier = new Supplier();
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getAvailNum() {
+        return availNum;
+    }
+
+    public void setAvailNum(String availNum) {
+        this.availNum = availNum;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /**
+     * @return the supplier
+     */
+    public Supplier getSupplier() {
+        return supplier;
+    }
+
+    /**
+     * @param supplier the supplier to set
+     */
+    public void setSupplier(Supplier supplier) {
+        this.supplier = supplier;
+    }
+    
+    
+}
---
+++ Create-View-Update-Products/src/Business/Supplier.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business;
+
+/**
+ *
+ * @author Tanmayee Kalluri
+ */
+public class Supplier {
+    private String supplier_name;
+    private String supplier_desc;
+    private String supplier_code;
+
+    /**
+     * @return the supplier_name
+     */
+    public String getSupplier_name() {
+        return supplier_name;
+    }
+
+    /**
+     * @param supplier_name the supplier_name to set
+     */
+    public void setSupplier_name(String supplier_name) {
+        this.supplier_name = supplier_name;
+    }
+
+    /**
+     * @return the supplier_desc
+     */
+    public String getSupplier_desc() {
+        return supplier_desc;
+    }
+
+    /**
+     * @param supplier_desc the supplier_desc to set
+     */
+    public void setSupplier_desc(String supplier_desc) {
+        this.supplier_desc = supplier_desc;
+    }
+
+    /**
+     * @return the supplier_code
+     */
+    public String getSupplier_code() {
+        return supplier_code;
+    }
+
+    /**
+     * @param supplier_code the supplier_code to set
+     */
+    public void setSupplier_code(String supplier_code) {
+        this.supplier_code = supplier_code;
+    }
+    
+}
